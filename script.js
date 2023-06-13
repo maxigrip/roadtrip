cd1ae401f891e8d7b397eca1acb268c8deba848e
maxigrip <121755322+maxigrip@users.noreply.github.com>
2023-06-13 20:46:52 +0200

    added pawthway from java to html

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function submitAnswer() {
     var userInput = document.getElementById("userInput").value;
     alert("You entered: " + userInput);
   }
+function checkAnswer() {
+  var userInput = document.getElementById("userInput").value;
+  if (userInput === "Göteborg") {
+    window.location.href = "correct.html";
+  } else {
+    window.location.href = "incorrect.html";
+  }
+}
