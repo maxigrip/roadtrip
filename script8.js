0a079c887c4ad7bd7709ade85ff15f98827f2ca5
maxigrip <121755322+maxigrip@users.noreply.github.com>
2023-06-14 13:01:50 +0200

    Delete script8.js

--- script8.js
+++
@@ -1,8 +0,0 @@
-function checkAnswer() {
-  var userInput = document.getElementById("userInput").value;
-  if (userInput === "Göteborg") {
-    window.location.href = "correct.html";
-  } else {
-    window.location.href = "incorrect.html";
-  }
-}
