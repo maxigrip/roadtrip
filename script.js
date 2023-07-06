eea8eb82bc32be5b361b37ec0e380f6df7372a80
maxigrip <121755322+maxigrip@users.noreply.github.com>
2023-07-06 10:17:40 +0200

    1

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function checkAnswer() {
     window.location.href = "incorrect.html";
   }
 }
+
+function checkNewAnswer() {
+  var userInput = document.getElementById("newAnswerInput").value;
+
+  if (userInput === "Göteborg") {
+    window.location.href = "correct.html";
+  } else {
+    window.location.href = "incorrect1.html";
+  }
+}
+
+function checkNewNewAnswer() {
+  var userInput = document.getElementById("checkNewNewAnswer").value;
+
+  if (userInput === "Göteborg") {
+    window.location.href = "correct.html";
+  } else {
+    window.location.href = "incorrect2.html";
+  }
+}
